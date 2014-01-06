e5b331b78f631e853eafd6d3787362f2d6106a94
Spencer Zhang <sepmein@gmail.com>
2014-01-07 07:56:32 +0800

    Improved the message system, adding the input function

--- app/scripts/controllers/controller.message.js
+++ app/scripts/controllers/controller.message.js
@@ -1,11 +1,22 @@
 'use strict';
 
 angular.module('hsArenaAnalysisApp')
-	.controller('hsArenaAnalysisApp.controllers.message', ['$rootScope','$scope', 'message',
-		function($rootScope, $scope) {
+	.controller('hsArenaAnalysisApp.controllers.message', ['$rootScope','$scope', '$timeout',
+		function($rootScope, $scope, $timeout) {
 			$rootScope.$on('message',function(event, msg, type){
 				$scope.message = msg;
 				$scope.type = type;
+				$scope.showMessage();
+				// hide message bar after 5s
+				$timeout(function(){
+					$scope.hideMessage();
+				}, 5000);
 			});
+			$scope.showMessage = function(){
+				$scope.show = 1;
+			};
+			$scope.hideMessage = function(){
+				$scope.show = 0;
+			};
 		}
 	]);
--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -13,13 +13,51 @@ angular.module('hsArenaAnalysisApp')
 
       $scope.$on('$firebaseAuth:login', function() {
         $scope.userEmail = $rootScope.auth.user.email;
+        message.success('登录成功');
+        // hide the login spinner
+        $scope.loginStatus = 0;
+
       });
 
       $scope.$on('$firebaseAuth:error', function(event, error) {
-        message.danger(error.code);
+        switch (error.code) {
+          case 'INVALID_PASSWORD':
+            message.warning('密码错误');
+            break;
+          case 'EMAIL_TAKEN':
+            message.warning('该email已注册');
+            break;
+          case 'INVALID_EMAIL':
+            message.warning('邮件格式不正确');
+            break;
+        }
+        // hide the login spinner
+        $scope.loginStatus = 0;
       });
-      $scope.login = function() {
-        authentication.login($scope.user.email, $scope.user.password, $scope.user.remenberMe);
+
+      $scope.loginStatus = 0;
+
+      $scope.login = function(e) {
+
+        this._login = function() {
+          authentication.login($scope.user.email, $scope.user.password, $scope.user.remenberMe);
+          // show the login spinner
+          $scope.loginStatus = 1;
+        };
+
+        if (e) {
+          // called by key press
+          if (e.which === 13) {
+            this._login();
+          }
+        } else {
+          this._login();
+        }
+
+      };
+
+      $scope.logink = function(e) {
+        console.log(e);
       };
 
       $scope.logout = function() {
